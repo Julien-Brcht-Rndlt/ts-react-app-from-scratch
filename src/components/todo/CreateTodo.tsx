ce23ef21da6a40c6d4dbcc5a324db7338867ab87
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-23 11:19:26 +0200

    Creation of Todo + Adding it to todos list

--- src/components/todo/CreateTodo.tsx
+++ src/components/todo/CreateTodo.tsx
@@ -1,38 +1,32 @@
-import React, { ChangeEvent, ChangeEventHandler, useState } from 'react';
-import Todo from '../../models/Todo';
+import React, { ChangeEvent, ChangeEventHandler, useState } from 'react'
+import Todo from '../../models/Todo'
 
 type CreateTodoProps = {
-    onCreatedTodo: (todo: Todo) => void;
+    onCreatedTodo: (todo: Todo) => void
 }
 
 const CreateTodo = ({ onCreatedTodo }: CreateTodoProps) => {
-
-    const [todo, setTodo] = useState<Todo>({
+    
+    const INITIAL_TODO_STATE = {
         task: '',
         done: false
-    });
+    }
 
-    /**
-     * 
-     * @param event
-     */
-    const createTodo: ChangeEventHandler<HTMLInputElement> = (event: ChangeEvent<HTMLInputElement>) => {
+    const [todo, setTodo] = useState<Todo>(INITIAL_TODO_STATE)
+
+    const createNewTodo: ChangeEventHandler<HTMLInputElement> = (event: ChangeEvent<HTMLInputElement>) => {
         setTodo({
             ...todo,
             task: event.target.value
         })
-       /*  setTodo({
-            task: '',
-            done: false
-        }); */
     }
-
+    
     return (
         <div>
-            <input value={todo.task} onChange={createTodo}/>
+            <input value={todo.task} onChange={createNewTodo}/>
             <button onClick={() => onCreatedTodo(todo)}>+</button>
         </div>
     );
 };
 
-export default CreateTodo;
+export default CreateTodo
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Todo from '../../models/Todo';
+import CreateTodo from './CreateTodo';
+import TodoList from './TodoList';
 
 const TodoContainer = () => {
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    /**
+     * Add a new todo to todos state (todo list state)
+     * @param todo a todo of type <Todo>
+     */
+    const addNewTodo = (todo: Todo) => {
+        setTodos([todo, ...todos]);
+    }
+
     return (
         <div>
-            
+            <CreateTodo onCreatedTodo={addNewTodo}/>
+            <TodoList/>
         </div>
     );
 };
