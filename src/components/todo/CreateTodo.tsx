99d5675d08f04bac129d74dff2705bc1ea89049a
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-23 10:45:53 +0200

    Creation of Todo + adding it to todos list

--- src/components/todo/CreateTodo.tsx
+++ src/components/todo/CreateTodo.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import Todo from '../../models/Todo';
 
-const CreateTodo = () => {
+type CreateTodoProps = {
+    onCreatedTodo: (todo: Todo) => void;
+}
+
+const CreateTodo = ({ onCreatedTodo }: CreateTodoProps) => {
+
+    const [todo, setTodo] = useState<Todo>({
+        task: '',
+        done: false
+    });
+
+    /**
+     * 
+     * @param event
+     */
+    const createTodo: ChangeEventHandler<HTMLInputElement> = (event: ChangeEvent<HTMLInputElement>) => {
+        setTodo({
+            ...todo,
+            task: event.target.value
+        })
+       /*  setTodo({
+            task: '',
+            done: false
+        }); */
+    }
+
     return (
         <div>
-            
+            <input value={todo.task} onChange={createTodo}/>
+            <button onClick={() => onCreatedTodo(todo)}>+</button>
         </div>
     );
 };
