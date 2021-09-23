e41798734598be6b99dbc4f18f61e1b69f69b4f8
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-09-23 12:14:54 +0200

    adding TodoContainer component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoContainer from './components/todo/TodoContainer';
 
 const App = () => {
   return (
     <div>
+      <TodoContainer/>
     </div>
   );
 }
